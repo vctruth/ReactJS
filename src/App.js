920aa44dab968e49bb5bb6b22cff8aef5d8857c2
chackov <33491480+chackov@users.noreply.github.com>
2018-01-21 23:42:24 -0500

    added some more style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import Child from  './child';
-import styled from 'styled-components';
+import  styled  from 'styled-components';
+import {injectGlobal } from 'styled-components';
 
+ injectGlobal `
+  body {
+    background-color: ghostwhite;
+    padding: 0;
+    margin: 0;
+    font-family: cursive;
+  }
+`
+const mainColor = 'indianred'
 const Title = styled.h1`
-  font-size: 1.5em;
-  color: purple;
+  color: ${props => props.color ||  'goldenrod'};
+  font-size: 2.8em;
+  margin: 25px;
+  padding-bottom: 20px;
+  border-bottom: 1px solid ${mainColor};
+  display: inline-block;
 `;
 const Head = styled.h3`
   font-size: 2em;
@@ -14,6 +28,23 @@ const Dtitle = styled.h1`
   font-size: 1.5em;
   color: ${props => props.primary ? 'blue' : 'indianred'};
 `;
+const Gallery = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Thumbnail = styled.img`
+  flex-grow: 1;
+  width: 50px;
+  height: 100px;
+  padding: 5px;
+  margin: 15px;
+  border: 1px solid ${mainColor};
+  border-radius: 8px;
+`;
+
+
+
 <Title>Hello World</Title> 
 
 class App extends React.Component {
@@ -32,15 +63,28 @@ class App extends React.Component {
   }
   
   render() {
+    
+    const thumbnails = Array.from({ length: 5 }, (_, index) => {
+    return <Thumbnail key={index + 1} src={require(`./thumbnails/images-${index + 1}.png`)} />;
+  });
+
     return (
-     <React.Fragment>
+     <React.Fragment className={this.props.className}>
         <Head> Testing sytled components </Head>
         <Child handlerFromParent={this.handleData} /> 
         <Title>Received from Child:<br />{this.state.fromChild}</Title>
         <Dtitle> this is dtitle </Dtitle>
+        <Gallery>
+          {thumbnails}
+        </Gallery>
+        
      </React.Fragment>
     );
   }
 }
 
-export default App;
+export default styled(App)`
+  width:  20vw;
+  height: 20vh;
+  text-align: center;
+`;
