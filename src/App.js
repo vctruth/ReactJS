c9a24677c383de0139ec9c8888179c9900bc1e34
chackov <33491480+chackov@users.noreply.github.com>
2018-01-21 21:42:56 -0500

    deleted app.css and added styled-components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ const Title = styled.h1`
   font-size: 1.5em;
   color: purple;
 `;
-
+const Head = styled.h3`
+  font-size: 2em;
+  color: goldenrod;
+`;
+const Dtitle = styled.h1`
+  font-size: 1.5em;
+  color: ${props => props.primary ? 'blue' : 'indianred'};
+`;
 <Title>Hello World</Title> 
 
 class App extends React.Component {
@@ -27,8 +34,10 @@ class App extends React.Component {
   render() {
     return (
      <React.Fragment>
+        <Head> Testing sytled components </Head>
         <Child handlerFromParent={this.handleData} /> 
         <Title>Received from Child:<br />{this.state.fromChild}</Title>
+        <Dtitle> this is dtitle </Dtitle>
      </React.Fragment>
     );
   }
